5e0ec43d6ca8bdefe58ee623f7f1d654b7696c08
Roseokpe <roseokpe@gmail.com>
2022-08-04 11:01:15 -0700

    add logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,60 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: '',
+      operation: '',
+    };
+    this.handleClicks = this.handleClicks.bind(this);
+  }
+
+  handleClicks(event) {
+    const targetValue = event.target.value;
+    this.setState((state) => calculate({
+      next: state.next,
+      total: state.total,
+      operation: state.operation,
+    }, targetValue));
   }
 
   render() {
+    const { total, operation, next } = this.state;
     return (
       <section>
         <div className="displayInputs">
           <p>
-            0
+            {total}
+            {' '}
+            {operation}
+            {' '}
+            {next}
+            {' '}
           </p>
         </div>
         <div className="calcDiv">
-          <button type="button" className="btn" value="AC">AC</button>
-          <button type="button" className="btn" value="+/-">+/-</button>
-          <button type="button" className="btn" value="%">%</button>
-          <button type="button" className="btn btnaction" value="÷">÷</button>
-          <button type="button" className="btn" value="7">7</button>
-          <button type="button" className="btn" value="8">8</button>
-          <button type="button" className="btn" value="9">9</button>
-          <button type="button" className="btn btnaction" value="x">x</button>
-          <button type="button" className="btn" value="4">4</button>
-          <button type="button" className="btn" value="5">5</button>
-          <button type="button" className="btn" value="6">6</button>
-          <button type="button" className="btn btnaction" value="-">-</button>
-          <button type="button" className="btn" value="1">1</button>
-          <button type="button" className="btn" value="2">2</button>
-          <button type="button" className="btn" value="3">3</button>
-          <button type="button" className="btn btnaction" value="+">+</button>
-          <button type="button" className="btnZero" value="0">0</button>
-          <button type="button" className="btn" value=".">.</button>
-          <button type="button" className="btn btnaction" value="=">=</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="AC">AC</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="+/-">+/-</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="%">%</button>
+          <button onClick={this.handleClicks} type="button" className="btn btnaction" value="÷">÷</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="7">7</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="8">8</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="9">9</button>
+          <button onClick={this.handleClicks} type="button" className="btn btnaction" value="x">x</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="4">4</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="5">5</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="6">6</button>
+          <button onClick={this.handleClicks} type="button" className="btn btnaction" value="-">-</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="1">1</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="2">2</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value="3">3</button>
+          <button onClick={this.handleClicks} type="button" className="btn btnaction" value="+">+</button>
+          <button onClick={this.handleClicks} type="button" className="btnZero" value="0">0</button>
+          <button onClick={this.handleClicks} type="button" className="btn" value=".">.</button>
+          <button onClick={this.handleClicks} type="button" className="btn btnaction" value="=">=</button>
         </div>
       </section>
     );
