721ee2f9b07d4b23c662671e82046308af261606
Roseokpe <roseokpe@gmail.com>
2022-08-10 16:52:38 -0700

    I update app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
-import Home from './components/home';
-import NavBar from './components/navBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Calculator from './components/calculator';
 import Quotes from './components/quote';
+import NavBar from './components/navbar';
+import Home from './components/home';
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/about" element={<Quotes />} />
-        <Route path="/topics" element={<calculator />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </>
+    );
+  }
 }
+export default App;
